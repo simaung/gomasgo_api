f1cb366a5dfe2088d3dfeccc77896da3cf1cb17d
simaung <ddhr00@gmail.com>
2019-08-06 11:48:04 +0700

    user operations

--- manage.py
+++ manage.py
@@ -6,8 +6,10 @@ from flask_script import Manager
 
 from app.main import create_app, db
 from app.main.model import user
+from app import blueprint
 
 app = create_app(os.getenv('BOILERPLATE_ENV') or 'dev')
+app.register_blueprint(blueprint)
 
 app.app_context().push()
 
